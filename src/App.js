6c62be57d3718e96a0d900ea76c968dbf46c1311
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-15 16:41:20 +0530

    App Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return <div className="" />
+    return (
+      <div className="">
+        <div className="pa3 f4 bg-dark white">APP HEADER</div>
+      </div>
+    )
   }
 }
 
