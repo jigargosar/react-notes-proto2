2e66fc2d35911369bbf06292b97fe5e1207be1ff
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-15 20:26:57 +0530

    MUI

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 import './pre-boot'
-import './post-boot'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'tachyons'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { ThemeProvider } from '@material-ui/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+
+function render() {
+  ReactDOM.render(
+    <ThemeProvider
+      theme={createMuiTheme({
+        typography: {
+          suppressWarning: true,
+        },
+      })}
+    >
+      <App />
+    </ThemeProvider>,
+    document.getElementById('root'),
+  )
+}
+
+render()
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
+
+if (module.hot) {
+  module.hot.accept(render)
+}
