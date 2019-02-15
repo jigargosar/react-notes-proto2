028231182dcebfd76d3ea20d7d94b085da3c52a2
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-15 16:52:09 +0530

    App Header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 
-function MenuIcon() {
-  return (
-    <div className="dib lh-solid " tabIndex={0}>
-      |||
-    </div>
-  )
+class MenuIcon extends Component {
+  render() {
+    return (
+      <div
+        className="dib lh-solid w2 h2 flex items-center justify-center"
+        tabIndex={0}
+      >
+        |||
+      </div>
+    )
+  }
 }
 
 class App extends Component {
   render() {
     return (
       <div className="">
-        <div className="pa3 f4 bg-dark white flex">
+        <div className="pa2 f4 bg-dark white flex items-center">
           <MenuIcon />
           <div className="lh-solid">APP HEADER</div>
         </div>
