7812cc5e5496a8f47a2efb4c95287e66254a9489
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-15 21:09:54 +0530

    MUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { withStyles } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { BottomAppBar } from './BottomAppBar'
 import { NoteList } from './NoteList'
 import { getNotes } from './Store'
+import { getCached, setCache } from './dom-helpers'
 
 const App = function() {
-  const [notes] = useState(getNotes)
+  const [notes] = useState(() => getCached('notes') || getNotes())
+
+  useEffect(() => {
+    setCache('notes', notes)
+  }, [notes])
+
   return (
     <Fragment>
       <CssBaseline />
