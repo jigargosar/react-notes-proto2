69a67fc91322d3ce71cd3770f6eccf4572e5f9ac
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-15 16:45:07 +0530

    App Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
+function MenuIcon() {
+  return (
+    <div className="dib pa1 lh-solid" tabIndex={0}>
+      |||
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
       <div className="">
-        <div className="pa3 f4 bg-dark white">APP HEADER</div>
+        <div className="pa3 f4 bg-dark white">
+          <MenuIcon />
+          APP HEADER
+        </div>
       </div>
     )
   }
