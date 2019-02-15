270c0c5ac78f275e03cd971745bf6e8051ca4a94
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-15 17:12:12 +0530

    App Header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
 /* disableInspection ES6UnusedImports */
 import React, { Component } from 'react'
 
-import { MenuSharp } from '@material-ui/icons'
+import {
+  Add as AddIcon,
+  MenuSharp as MenuIcon,
+  MoreVert as MoreIcon,
+  Search as SearchIcon,
+} from '@material-ui/icons'
+import {
+  AppBar,
+  Fab,
+  IconButton,
+  Toolbar,
+  withStyles,
+} from '@material-ui/core'
+
+function styles() {
+  return {
+    appBar: {
+      top: 'auto',
+      bottom: 0,
+    },
+    toolbar: {
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    fabButton: {
+      position: 'absolute',
+      zIndex: 1,
+      top: -30,
+      left: 0,
+      right: 0,
+      margin: '0 auto',
+    },
+  }
+}
 
 class App extends Component {
   render() {
+    const { classes } = this.props
     return (
-      <div className="">
-        <div className="pa2 f4 bg-dark white flex items-center">
-          <MenuSharp />
-          <div className="lh-solid">APP HEADER</div>
-        </div>
-      </div>
+      <>
+        <AppBar
+          position="fixed"
+          color="primary"
+          className={classes.appBar}
+        >
+          <Toolbar className={classes.toolbar}>
+            <IconButton color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Fab
+              color="secondary"
+              aria-label="Add"
+              className={classes.fabButton}
+            >
+              <AddIcon />
+            </Fab>
+            <div>
+              <IconButton color="inherit">
+                <SearchIcon />
+              </IconButton>
+              <IconButton color="inherit">
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </>
     )
   }
 }
 
-export default App
+export default withStyles(styles)(App)
