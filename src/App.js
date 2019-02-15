72bfb1762fcd79534341a9e06ffbfa884c079252
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-15 21:18:48 +0530

    MUI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useReducer, useState } from 'react'
 import { withStyles } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { BottomAppBar } from './BottomAppBar'
@@ -6,12 +6,31 @@ import { NoteList } from './NoteList'
 import { getNotes } from './Store'
 import { getCached, setCache } from './dom-helpers'
 
+function initNotes(maybeState) {
+  return maybeState || getNotes()
+}
+
+function notesReducer(state, action) {
+  switch (action.type) {
+    case 'addNew':
+      return state
+    case 'delete':
+      return state
+    case 'reset':
+      return initNotes(action.payload)
+    default:
+      throw new Error('Invalid Action')
+  }
+}
+
 const App = function() {
   const [notes] = useState(() => getCached('notes') || getNotes())
 
   useEffect(() => {
     setCache('notes', notes)
   }, [notes])
+
+  useReducer(notesReducer, getCached('notes'), initNotes)
 
   return (
     <Fragment>
